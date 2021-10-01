d2e198a47caaf214f95d051b99a8340f0a1ad470
Alistair Smith <alistairsmith01@gmail.com>
2021-10-01 16:31:24 +0100

    fix: No recursive imports

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,4 +1,5 @@
-import {Join, Split} from '.';
+import {Join} from './join';
+import {Split} from './split';
 import {Reverse as ReverseTuple} from './utility';
 
 export type Reverse<Str extends string> = Join<ReverseTuple<Split<Str, ''>>, ''>;
